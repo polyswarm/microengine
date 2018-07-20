53f9ba3a373c8686ff842a194f0eeb75a828f664
Steve Bassi <sb@polyswarm.io>
2018-07-20 00:10:28 -0400

    Updated backend loader to (optionally) take custom class name

--- src/microengine/backend.py
+++ src/microengine/backend.py
@@ -10,7 +10,7 @@ def choose_backend(backend):
 
     Args:
         backend (str): Name of the backend to load, either one of the predefined
-            implementations or the name of a module to load
+            implementations or the name of a module to load (module:ClassName syntax or default of module:Microengine)
     Returns:
         (Class): Microengine class of the selected implementation
     Raises:
@@ -25,9 +25,12 @@ def choose_backend(backend):
     elif backend == 'clamav':
         micro_engine_class = ClamavMicroengine  # ClamavMicroengine(polyswarmd_addr, keyfile, password).run(testing)
     else:
-        micro_engine_module = importlib.import_module(backend)
-        micro_engine_class = micro_engine_module.Microengine
-        # todo make the class name customizable
+        import_l = backend.split(":")
+        micro_engine_module_s = import_l[0]
+
+        micro_engine_module = importlib.import_module(micro_engine_module_s)
+        micro_engine_class = micro_engine_module.Microengine if ":" not in backend else eval(
+            "micro_engine_module.{0}".format(import_l[1]))
 
     if micro_engine_class is None:
         raise Exception("No microengine backend found {0}".format(backend))
