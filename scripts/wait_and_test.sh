45c0f9920ef6fe52a057430dcdcbeff21779671f
Rob Lathrop <rl@polyswarm.io>
2018-08-28 16:59:34 -0400

    Get API key on start.

--- scripts/wait_and_test.sh
+++ scripts/wait_and_test.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-./scripts/wait_for_it.sh $POLYSWARMD_HOST:$POLYSWARMD_PORT -t 0
-microengine --testing 10 --keyfile docker/keyfile --password password --backend scratch
+./scripts/wait_and_run.sh --testing 10 --keyfile docker/keyfile --password password --backend scratch
